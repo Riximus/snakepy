a1b804c7bb1bb034852d71f12410e13ba7d34c3a
Sami Elsayed <90798961+selsayed25@users.noreply.github.com>
2022-05-15 15:28:50 -0400

    Update SnakeGame.py

--- SnakeGame.py
+++ SnakeGame.py
@@ -27,7 +27,7 @@ else:
 
 
 # Initialise game window
-pygame.display.set_caption('Snake Eater')
+pygame.display.set_caption('Snake')
 game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
 
 
